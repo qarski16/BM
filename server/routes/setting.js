99f937641fccff575ca0256326e0d7e1baa8656c
qarski16 <riqar16@gmail.com>
2026-06-26 02:18:00 +0800

    Update setting.js

--- server/routes/setting.js
+++ server/routes/setting.js
@@ -8,35 +8,39 @@ router.get('/', async (req, res) => {
   try {
     let setting = await Setting.findOne();
     if (!setting) {
-      // Jika database kosong, buat dokumen default pertama kali
       setting = new Setting({});
       await setting.save();
     }
     res.json(setting);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error saat mengambil pengaturan');
+    res.status(500).json({ message: 'Server Error saat mengambil pengaturan' });
   }
 });
 
 // @route   POST /api/setting
 // @desc    Simpan atau perbarui konfigurasi global
 router.post('/', async (req, res) => {
+  const { namaAplikasi, alamatToko, kontak, tarifPerKm } = req.body;
+  
+  // ✅ PERBAIKAN: Validasi input sebelum proses
+  if (!namaAplikasi) {
+    return res.status(400).json({ message: 'Nama aplikasi wajib diisi' });
+  }
+  
   try {
     let setting = await Setting.findOne();
     
     if (setting) {
-      // Perbarui data yang sudah ada
       setting = await Setting.findOneAndUpdate({}, req.body, { new: true });
     } else {
-      // Buat baru jika belum ada
       setting = new Setting(req.body);
       await setting.save();
     }
     res.json({ msg: 'Pengaturan sistem berhasil disimpan!', data: setting });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error saat menyimpan pengaturan');
+    res.status(500).json({ message: 'Server Error saat menyimpan pengaturan' });
   }
 });
 
